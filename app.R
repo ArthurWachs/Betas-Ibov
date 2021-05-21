75a5cb7620cc11d771d6b26e5db921989b8b002c
ArthurWachs <arthurwachs@gmail.com>
2021-05-20 22:22:39 -0300

    Update app.R

--- app.R
+++ app.R
@@ -1,11 +1,3 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
 library(shiny)
 library(BatchGetSymbols)
@@ -17,14 +9,11 @@ library(ggpmisc)
 acoes <- BatchGetSymbols::GetIbovStocks()$tickers
 acoes <- paste0(acoes, ".SA")
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
  
 
-    # Application title
     titlePanel("Beta"),
 
-    # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
             selectInput("input_acoes",
@@ -38,14 +27,12 @@ ui <- fluidPage(
                         value = 365)
         ),
 
-        # Show a plot of the generated distribution
         mainPanel(
            plotOutput("out")
         )
     )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
 
     bench <- "^BVSP"
